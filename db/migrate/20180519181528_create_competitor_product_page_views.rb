8207610178830669475480d47fe422d93d872329
MidnightBlue <isfore@gmail.com>
2018-05-20 05:17:02 +0800

    page view

--- db/migrate/20180519181528_create_competitor_product_page_views.rb
+++ db/migrate/20180519181528_create_competitor_product_page_views.rb
@@ -1,4 +1,7 @@
 class CreateCompetitorProductPageViews < ActiveRecord::Migration[5.2]
+
+  # class Competitor::Product < ApplicationRecord; end
+
   def change
     create_table :competitor_product_page_views do |t|
       t.integer :product_id
@@ -12,12 +15,12 @@ class CreateCompetitorProductPageViews < ActiveRecord::Migration[5.2]
   end
 
   def data
-    Competitor::Product::PageView.create!({
-      product_id: Competitor::Product.first.id,
-      uuid: 'bf714d4cf8d9c25f51b3bed6ea94f1c2e3cf898f1ee7abdbb1b53a2fce8df',
-      name: '有豐食品 北海鱈魚香絲 大包裝(600g)',
-      price: 420,
-      quantity: nil
-    })
+    # Competitor::Product::PageView.create!({
+    #   product_id: Competitor::Product.first.id,
+    #   uuid: 'bf714d4cf8d9c25f51b3bed6ea94f1c2e3cf898f1ee7abdbb1b53a2fce8df',
+    #   name: '有豐食品 北海鱈魚香絲 大包裝(600g)',
+    #   price: 420,
+    #   quantity: nil
+    # })
   end
 end
